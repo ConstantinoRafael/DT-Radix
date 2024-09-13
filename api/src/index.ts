8aeed25b907af83e4a245a4e25d7c99619eb5315
Rafael <rafael.constantino89@gmail.com>
2024-09-12 21:16:29 -0300

    feat: add express initial server

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,13 @@
-console.log("hellow");
+import express from "express";
+import { config } from "dotenv";
+
+config();
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("helooow");
+});
+
+app.listen(port, () => console.log(`listening on port ${port}`));
