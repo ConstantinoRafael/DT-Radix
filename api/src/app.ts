8a738260d3e860e843de4ed01a0c8a877aaac77e
Rafael <rafael.constantino89@gmail.com>
2024-09-14 06:13:33 -0300

    feat: model sensor reading

--- api/src/app.ts
+++ api/src/app.ts
@@ -4,10 +4,11 @@ import { config } from "dotenv";
 config();
 const app = express();
 
-const port = process.env.PORT || 5000;
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("helooow");
 });
 
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`listening on port ${port}`));
---
+++ api/src/models/SensorReading.ts
@@ -0,0 +1,18 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface ISensorReading extends Document {
+  equipmentId: string;
+  timestamp: Date;
+  value: number;
+}
+
+const SensorReadingSchema: Schema = new Schema({
+  equipmentId: { type: String, required: true },
+  timestampe: { type: Date, required: true },
+  value: { type: Number, required: true },
+});
+
+export default mongoose.model<ISensorReading>(
+  "SensorReading",
+  SensorReadingSchema
+);
